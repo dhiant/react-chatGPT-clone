543ba88e2678670f5d95b2b01dd48fd56bc7cb40
sushant dhimal <sushant07.dhimal@gmail.com>
2023-01-31 23:00:40 +0545

    feat: add landing page content - brief description of the app - a list of features

---
+++ client/src/components/IntroSection.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const IntroSection = () => {
+  return (
+    <div id="introsection">
+      <h1>Introducing Talkbot - The Ultimate AI Assistant</h1>
+      <h2>
+        A cutting-edge AI-powered app that provides instant answers to any
+        question you may have. With Talkbot, you'll never be left searching for
+        answers again. Whether you need information for school or work, or just
+        want to know the latest news, Talkbot has you covered.
+      </h2>
+      <p>
+        Features:
+        <ul>
+          <li>Instant answers to any question</li>
+          <li>Deep learning technology that improves with usage</li>
+          <li>Continuously Learning</li>
+          <li>User-friendly interface</li>
+          <li>Available 24/7</li>
+        </ul>
+      </p>
+      <p>
+        Say goodbye to endless searching and typing, and say hello to TalkBot,
+        your personal AI assistant. Try it now and see for yourself how TalkBot
+        can make your life easier.
+      </p>
+    </div>
+  );
+};
+
+export default IntroSection;
